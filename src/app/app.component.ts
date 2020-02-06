c8850545b5a775a78997eec01023fccd03f23180
choyzer <choyzer@ptc.com>
2020-02-06 12:04:34 +0200

    sdfsdfsdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {catchError, map} from 'rxjs/operators';
+import {Observable, throwError} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'auth-test';
+
+  constructor(
+    private http: HttpClient,
+  ) {
+
+  }
+
+  private formatErrors(error: any) {
+    return throwError(error.error);
+  }
+
+
+  post(): Observable<any> {
+    return this.http.post<any>(`http://localhost:8080/device/verify`, {user_code: 'asdasdasdad'})
+      .pipe(map(res => {
+        console.log(res);
+      }));
+  }
+
+  redirect() {
+    this.post().subscribe(x => {
+      console.log(x);
+    });
+  }
+
 }
